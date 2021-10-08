7bb48b0c9b2ca9dfd6615192cf622b897343ee9b
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2021-10-08 16:19:39 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -13,10 +13,27 @@ const firebaseConfig = {
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
 
+    function addRoom(){
+      room_name= document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+    }
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+   console.log("Room name -" + Room_names);
+   row = "<div class='room_name' id=" + Room_names + "onclick= 'redirectToRoomNames(this.id)'>#" + Room_names + "</div><hr>";
+   document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+
+function redirectToRoomNames(name){
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
